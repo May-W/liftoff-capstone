08817cf968f6ccf1ca36e35b27c77b0a03e0e10b
May W <51608792+may-w@users.noreply.github.com>
2019-11-04 18:11:53 -0600

    added information from studio 3.9 in LC101 - user signup - to try to get somethign i can use

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,8 +1,11 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UserController {
@@ -11,11 +14,22 @@ public class UserController {
     public String index() {
         return "etc/login";
     }
-    @RequestMapping(value = "signup")
+    @RequestMapping(value = "signup", method = RequestMethod.GET)
     public String signup() {
         return "etc/signup";
     }
 
+    @RequestMapping(value = "signup", method = RequestMethod.POST)
+    public String add(Model model, @ModelAttribute User user, String verify_password) {
+        model.addAttribute("user", user);
+
+        if (user.getPassword().equals(verify_password)) {
+            return "etc/index";
+        }
+
+        model.addAttribute("error_message", "I'm sorry, but password doesn't match verify password. Please try again.");
+        return "etc/signup";
+    }
 
 }
 
